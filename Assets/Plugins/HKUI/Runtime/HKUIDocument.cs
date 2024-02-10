35739dc16735ab649b243a668b2f89b0dd187dc0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-10 17:59:04 +0900

    新しいプラグインとランタイムファイルを追加しました

---
+++ Assets/Plugins/HKUI/Runtime/HKUIDocument.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HK.UI
+{
+    public class HKUIDocument : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] elements;
+
+        private readonly Dictionary<string, GameObject> elementMap = new();
+
+        private readonly Dictionary<GameObject, Dictionary<Type, Component>> componentMap = new();
+
+        public T Q<T>(string name) where T : Component
+        {
+            if (elementMap.Count == 0)
+            {
+                foreach (var element in elements)
+                {
+                    elementMap[element.name] = element;
+                }
+            }
+
+            if (elementMap.TryGetValue(name, out var e))
+            {
+                if (!componentMap.TryGetValue(e, out var c))
+                {
+                    c = new Dictionary<Type, Component>();
+                    componentMap[e] = c;
+                }
+
+                if (!c.TryGetValue(typeof(T), out var t))
+                {
+                    t = e.GetComponent<T>();
+                    c[typeof(T)] = t;
+                }
+
+                return (T)t;
+            }
+            else
+            {
+                throw new Exception($"Element not found: {name}");
+            }
+        }
+    }
+}
