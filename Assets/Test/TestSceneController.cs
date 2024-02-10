55e8bee0e8ea785b9895553e172950f8248b9f62
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-10 18:11:07 +0900

    コードの変更を記述するための短く簡潔なコミットメッセージを提案します。*** ***コミットメッセージは、以下のロケールに翻訳される必要があります: ja

--- Assets/Test/TestSceneController.cs
+++ Assets/Test/TestSceneController.cs
@@ -1,8 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using HK.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestSceneController : MonoBehaviour
 {
+    [SerializeField]
+    private HKUIDocument documentPrefab;
 
+    void Start()
+    {
+        var document = Instantiate(documentPrefab);
+        document.Q<TMP_Text>("Header").text = "Hello, World!";
+        var button = document.Q<Button>("Button");
+        button.onClick.AddListener(() =>
+        {
+            Debug.Log("Button clicked");
+        });
+    }
 }
